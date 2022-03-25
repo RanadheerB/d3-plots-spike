8779852aa504dc373b2f70845af2d2adc3bc5dff
RanadheerB <Ranadheer.Bolli@bakerhughes.com>
2022-03-25 19:22:17 +0530

    added tick to basic cursor and modified harmonic

--- src/app/waveform/waveform.component.ts
+++ src/app/waveform/waveform.component.ts
@@ -4,7 +4,7 @@ import * as d3Scale from 'd3-scale';
 import * as d3Shape from 'd3-shape';
 import * as d3Array from 'd3-array';
 import * as d3Axis from 'd3-axis';
-import { WaveformData } from "../shared/machine-data"
+import { SpectrumData } from "../shared/spectrum-data"
 import * as d3 from 'd3';
 import { BehaviorSubject } from 'rxjs';
 
@@ -22,14 +22,20 @@ export class WaveformComponent implements OnInit {
  private svg: any;
  map1 = new Map;
  X : any;
+Y: any;
  cursor_readout:any;
  basic_cursor_A: any;
  basic_cursor_B: any;
  current_cursor: any;
  c_id:number = 0;
-cursor_point = new BehaviorSubject<number>(0);
+cursor_event_A = new BehaviorSubject<number>(0);
+cursor_event_B = new BehaviorSubject<number>(0);
  index:any;
- isHarmonic: boolean = false;
+ isHarmonic_A: boolean = false;
+ isHarmonic_B: boolean = false;
+ highest_X = 0;
+ highest_Y = 0;
+
  private line: d3Shape.Line<[number, number]> | undefined;
 
  constructor() {
@@ -47,6 +53,14 @@ cursor_point = new BehaviorSubject<number>(0);
  }
 
  private initSvg() {
+   this.X=SpectrumData.map((data)=>data.x_value)
+   this.Y=SpectrumData.map((data)=>data.y_value)
+
+   SpectrumData.forEach((data:any)=>{
+     this.highest_X = Math.max(this.highest_X,data.x_value);
+     this.highest_Y = Math.max(this.highest_Y,data.y_value);
+   })
+
   this.svg = d3Selection
    .select('svg')
    .on("click", (event:any)=>this.mouseClick(event))
@@ -55,26 +69,30 @@ cursor_point = new BehaviorSubject<number>(0);
     'transform',
     'translate(' + this.margin.left + ',' + this.margin.top + ')'
    );
+
    }
 
  private initAxis() {
   this.x = d3Scale.scaleLinear().range([0, this.width]);
   this.y = d3Scale.scaleLinear().range([this.height, 0]);
-  this.x.domain(d3Array.extent(WaveformData, (d) => d.x_value));
-  this.y.domain(d3Array.extent(WaveformData, (d) => d.y_value));
+  this.x.domain(d3Array.extent(SpectrumData, (d) => d.x_value));
+  this.y.domain(d3Array.extent(SpectrumData, (d) => d.y_value));
+
  }
 
  private drawAxis() {
   this.svg
    .append('g')
-   .attr('class', 'axis axis--x')
+   .attr('class', 'Xaxis')
    .attr('transform', 'translate(0,' + this.height + ')')
-   .call(d3Axis.axisBottom(this.x));
+   .call(
+     d3Axis.axisBottom(this.x))
 
   this.svg
    .append('g')
-   .attr('class', 'axis axis--y')
+   .attr('class', 'Yaxis')
    .call(d3Axis.axisLeft(this.y))
+
  }
 
  private drawLine() {
@@ -85,55 +103,90 @@ cursor_point = new BehaviorSubject<number>(0);
 
   this.svg
     .append('path')
-    .datum(WaveformData)
+    .datum(SpectrumData)
     .attr('class', 'line')
     .attr("d", this.line)
     .style("fill", "none")
-    .style("stroke", "#CC0000")
+    .style("stroke", "blue")
     .style("stroke-width", "1");
  }
 
  public mouseClick = (event:any) => {
 
-  let i=0;
-   this.X=WaveformData.map((data)=>data.x_value)
-
-   WaveformData.forEach(data=>{
-     this.map1.set(data.x_value,i++)
-   })
-  //  this.cursor_point.next(d3.pointer(event)[0]-49);
    console.log(d3.pointer(event)[0]-49,"pointer event");
 
-    this.index = d3.bisectCenter(this.X, this.x.invert(d3.pointer(event)[0]-49));
+     this.index = d3.bisectCenter(this.X, this.x.invert(d3.pointer(event)[0]-49));
     console.log(this.index,"basic");
 
     const text = d3.select('.readout');
-    const Xaxis = WaveformData[this.index].x_value;
-    const Yaxis = WaveformData[this.index].y_value;
-    text.text(`X : ${WaveformData[this.index].x_value} Y: ${WaveformData[this.index].y_value} `);
+    const Xaxis = SpectrumData[this.index].x_value;
+    const Yaxis = SpectrumData[this.index].y_value;
+    text.text(`X : ${SpectrumData[this.index].x_value} Y: ${SpectrumData[this.index].y_value} `);
 
-    if(this.c_id == 0)
+     let x_TickClass="",y_TickClass="",tickColor="";
+    if(this.c_id == 0){
+    tickColor = "orange";
+   this.cursor_event_A.next(Xaxis);
     this.current_cursor = this.basic_cursor_A;
-    else
+    }
+    else{
+    tickColor = "green";
+   this.cursor_event_B.next(Xaxis);
     this.current_cursor = this.basic_cursor_B;
+    }
+    this.current_cursor.attr("x",this.x(SpectrumData[this.index].x_value)-5)
+    .attr("y", this.y(SpectrumData[this.index].y_value)-5)
 
-    this.current_cursor.attr("x",this.x(WaveformData[this.index].x_value)-5)
-    .attr("y", this.y(WaveformData[this.index].y_value)-5)
-    console.log("X:",WaveformData[this.index].x_value, "Y:", WaveformData[this.index].y_value);
-   this.cursor_point.next(Xaxis);
+    console.log("X:",SpectrumData[this.index].x_value, "Y:", SpectrumData[this.index].y_value);
+
+     x_TickClass = "x-Axis"+this.c_id;
+     y_TickClass = "y-Axis"+this.c_id;
+
+    this.svg.select("."+x_TickClass).remove();
+    this.svg.select("."+y_TickClass).remove();
+
+     this.svg
+    .select(".Xaxis")
+    .append("g")
+    .attr('class', x_TickClass)
+   .call(d3Axis.axisBottom(this.x).tickSize(10).tickValues([Xaxis]))
+    .select("line")
+   .style("stroke", tickColor)
+   .style("stroke-width", "2")
+
+   this.svg.select("."+x_TickClass+" text")
+     .style("fill", "none")
+
+     this.svg
+    .select(".Yaxis")
+    .append("g")
+    .attr('class', y_TickClass)
+   .call(d3Axis.axisLeft(this.y).tickSize(10).tickValues([Yaxis]))
+    .select("line")
+   .style("stroke", tickColor)
+   .style("stroke-width", "2")
+
+   this.svg.select("."+y_TickClass+" text")
+     .style("fill", "none")
+
   };
 
   private renderCursor(){
+    console.log(this.highest_Y,"highrst");
+    console.log(this.Y);
+
+    let cursorA_index = d3.bisectCenter(this.Y, this.highest_Y);
+    let cursorB_index = d3.bisectCenter(this.Y, this.highest_Y);
+
+    console.log(cursorA_index);
+
     this.basic_cursor_A = this.svg.append('g')
-    .selectAll(".cursor")
-    .data([1])
-    .enter()
     .append("image")
     .attr("xlink:href",'../../assets/starburst.svg')
     .style("width","10px")
     .style("height","10px")
-    .attr('x',this.x(WaveformData[80].x_value)-5)
-    .attr('y',this.y(WaveformData[80].y_value)-5)
+    .attr('x',this.x(SpectrumData[16].x_value)-5)
+    .attr('y',this.y(SpectrumData[16].y_value)-5)
 
      this.basic_cursor_B = this.svg.append('g')
     .append("image")
@@ -141,43 +194,74 @@ cursor_point = new BehaviorSubject<number>(0);
     .style("width","10px")
     .style("height","10px")
     .style("fill","red")
-    .attr('x',this.x(WaveformData[160].x_value)-5)
-    .attr('y',this.y(WaveformData[160].y_value)-5)
+    .attr('x',this.x(SpectrumData[cursorB_index].x_value)-5)
+    .attr('y',this.y(SpectrumData[cursorB_index].y_value)-5)
 
   }
 
- public ToggleEnable(){
+ public ClickCursorHarmonicA(){
     let harmonic_index = 0;
-    // let index_1:any,index_2:any;
-    this.isHarmonic = !this.isHarmonic;
+    this.isHarmonic_A = !this.isHarmonic_A;
+    if(!this.isHarmonic_A){
+         this.svg.
+        selectAll(".cursor_A")
+        .remove()
+    }
      let c = 1;let image_link: string='';
      let harmonic_cursor;
-      this.cursor_point.subscribe(value=>{
-         if(this.isHarmonic){
-        this.svg.
-        selectAll("basic")
+      this.cursor_event_A.subscribe(data=>{
+         if(this.isHarmonic_A){
+          this.svg.
+        selectAll(".cursor_A")
         .remove()
         c=1;
-        harmonic_index = 0;
-        if(this.c_id==0){
+           harmonic_index = 0;
            image_link ='../../assets/starburst.svg';
-        }
-        else{
-          image_link = '../../assets/machine.svg';
-        }
-        console.log(this.X.length);
-
-    while(harmonic_index<2000){
-     harmonic_index = d3.bisectCenter(this.X, value*c);
-     harmonic_cursor = this.svg.append('g')
+    while(harmonic_index<800){
+     harmonic_index = d3.bisectCenter(this.X, c*data);
+     harmonic_cursor = this.svg
     .append("image")
-    .attr('class', 'basic')
+    .attr('class', 'cursor_A')
     .attr("xlink:href",image_link)
     .style("width","10px")
     .style("height","10px")
-    .attr('x',(this.x(WaveformData[harmonic_index].x_value)-5))
-    .attr('y',(this.y(WaveformData[harmonic_index].y_value)-5))
-    console.log("X:",WaveformData[harmonic_index].x_value, "Y:", WaveformData[harmonic_index].y_value);
+    .attr('x',(this.x(SpectrumData[harmonic_index].x_value)-5))
+    .attr('y',(this.y(SpectrumData[harmonic_index].y_value)-5))
+    console.log("X:",SpectrumData[harmonic_index].x_value, "Y:", SpectrumData[harmonic_index].y_value);
+    c++;
+    }
+  }
+  })
+  }
+
+  public ClickCursorHarmonicB(){
+
+    this.isHarmonic_B = !this.isHarmonic_B;
+     if(!this.isHarmonic_B){
+         this.svg.
+        selectAll(".cursor_B")
+        .remove()
+    }
+      this.cursor_event_B.subscribe(data=>{
+          const image_link = '../../assets/machine.svg';
+          if(this.isHarmonic_B){
+         this.svg.
+        selectAll(".cursor_B")
+        .remove()
+          let c=2;
+           let harmonic_index = 0;
+        console.log(this.X.length);
+    while(harmonic_index<800){
+     harmonic_index = d3.bisectCenter(this.X, c*data);
+     let harmonic_cursor = this.svg
+    .append("image")
+    .attr('class', 'cursor_B')
+    .attr("xlink:href",image_link)
+    .style("width","10px")
+    .style("height","10px")
+    .attr('x',(this.x(SpectrumData[harmonic_index].x_value)-5))
+    .attr('y',(this.y(SpectrumData[harmonic_index].y_value)-5))
+    console.log("X:",SpectrumData[harmonic_index].x_value, "Y:", SpectrumData[harmonic_index].y_value);
     c++;
     }
   }
